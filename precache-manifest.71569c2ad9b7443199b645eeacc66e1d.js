4c351f872e9535cf6b9f7226b0eda812fbb41e27
Dalibor Ristic <daliborristic883@gmail.com>
2023-01-13 09:02:35 +0100

    Updates

---
+++ precache-manifest.71569c2ad9b7443199b645eeacc66e1d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffca5854fb5da2cd9d42f865a6549be",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "8dd2330e72cf675b843b",
+    "url": "/profile/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e278149619a2af81f158",
+    "url": "/profile/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "6da55b036c6be1fffd71",
+    "url": "/profile/static/css/main.26a59920.chunk.css"
+  },
+  {
+    "revision": "6c1d09018538913ac3a1",
+    "url": "/profile/static/js/2.2da88be1.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/profile/static/js/2.2da88be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd2330e72cf675b843b",
+    "url": "/profile/static/js/3.e4393979.chunk.js"
+  },
+  {
+    "revision": "e278149619a2af81f158",
+    "url": "/profile/static/js/4.4897c883.chunk.js"
+  },
+  {
+    "revision": "6da55b036c6be1fffd71",
+    "url": "/profile/static/js/main.a7b67969.chunk.js"
+  },
+  {
+    "revision": "14af5a220dd12e2071e8",
+    "url": "/profile/static/js/runtime-main.2ef0265a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/profile/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/profile/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/profile/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/profile/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "e3a410d54ad8fe15413dfe37eea090f2",
+    "url": "/profile/static/media/dcsLogo.e3a410d5.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/profile/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "394974a9af483728301642d1515985b3",
+    "url": "/profile/static/media/facultyPhiLogo.394974a9.png"
+  },
+  {
+    "revision": "a4427df08e89e79cbd158c828391a9ea",
+    "url": "/profile/static/media/fbeLogo.a4427df0.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/profile/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5650d4fa50d156e12fd98bdb24ee5848",
+    "url": "/profile/static/media/helloLogo.5650d4fa.png"
+  },
+  {
+    "revision": "41c7ab5e9ec7c83a93890e4e3c670b93",
+    "url": "/profile/static/media/helloreportLogo.41c7ab5e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/profile/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b2f101ed45151c12adc9b26ff6115ad3",
+    "url": "/profile/static/media/miloscicLogo.b2f101ed.png"
+  },
+  {
+    "revision": "4c16d8eeb28d30bc80a6a62d772a908c",
+    "url": "/profile/static/media/nisUniLogo.4c16d8ee.png"
+  },
+  {
+    "revision": "d4f9a940741ef2ce704d0e2cb54aeb72",
+    "url": "/profile/static/media/nuomLogo.d4f9a940.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/profile/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/profile/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "f0c7a39ecec4beda5499a8b2d110ce68",
+    "url": "/profile/static/media/talkitoutLogo.f0c7a39e.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/profile/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c50f92adfab5a4b16f489f84616a130a",
+    "url": "/profile/static/media/upliftLogo.c50f92ad.png"
+  }
+]);
